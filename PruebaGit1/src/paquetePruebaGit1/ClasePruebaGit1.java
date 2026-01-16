358617e0b19d02c046bfacdd84a4e69ba51e7ef0
Jaime Gamero Matos <gameromatos@gmail.com>
2026-01-16 13:58:09 +0100

    esta es la primera version de este archivo

---
+++ PruebaGit1/src/paquetePruebaGit1/ClasePruebaGit1.java
@@ -0,0 +1,10 @@
+package paquetePruebaGit1;
+
+public class ClasePruebaGit1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+	}
+
+}
